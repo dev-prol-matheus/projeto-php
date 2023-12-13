eeb3fb2518e223cc6189e561156fe4736fce421e
Glauber <grfs@discente.ifpe.edu.br>
2023-12-13 20:55:29 -0300

    Atualização do arquivo de login e um novo arquivo 'usuários'

---
+++ php/usuarios.php
@@ -0,0 +1,23 @@
+<?php
+include "../conexao/conexao.php";
+try {   
+  
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    $senhaCripto = password_hash($senha, PASSWORD_DEFAULT);
+      
+    $sql = "INSERT INTO usuarios (login, senha) VALUES (:login, :senha)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':login', $login);
+    $stmt->bindParam(':senha', $senhaCripto);
+    
+    $stmt->execute();
+
+    header("Location: ../"); // dependendo do arquivo "listar kits em uso"
+    exit();
+
+} catch(PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
