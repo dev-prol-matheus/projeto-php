cf636bd8a7d2e9a1308cd611c86797a8572d3420
Gustavo <gustavopereiraaa01@gmail.com>
2023-12-15 20:04:18 -0300

    ajustes na tela de login e no footer

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
-<html>
-<head> 
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Kits - Senac</title>
     <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estilo-cadrastocolaboradores.css">
 </head>
-<body>    
+<body>
+
     <header>
-        <h1>Projeto Kits</h1>
-        <?php include "includes/menu.php" ?>
+        <h1>Solicitação de kits</h1>
+       
     </header>
+
+    <section>
+        <div class="container">
+        <form id="LoginForm" class="form">
+            <input type="text" id="username" placeholder="Usuário" required><br><br>
+            <input type="password" id="password" placeholder="Senha" required><br><br>
+            <button type="submit">Entrar</button>
+        
+        </div>
+
         <?php include "includes/footer.php" ?>
 </body>
 </html>
