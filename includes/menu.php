bc2549787969793b4d51999c579f02afa192b778
Rafael <rfreitas.social@gmail.com>
2023-12-15 20:54:32 -0300

    push

--- includes/menu.php
+++ includes/menu.php
@@ -3,6 +3,7 @@
         <li><a href="Listadecolaboradores.php">Colaboradores</a></li>
         <li><a href="cadastrarcColaboradores.php">Cadastrar Colaboradores</a></li>
         <li><a href="solicitacao.php">Solicitação de Kits</a></li>
+        <li><a href="emUso.php">Kits em Uso</a></li>
         <li><a href="#">Usuários</a></li>
         <li><a href="#"> Sair</a></li>
       </ul>
