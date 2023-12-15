92a740748921856c977f8932dc54d768cbc269d0
joas <joasemiliano1245@gmail.com>
2023-12-14 21:13:37 -0300

    criaçao da de login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Solicitação de kit -senac</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+    <header>
+        <h1>Solicitação de kits</h1>
+       
+    </header>
+
+    <section>
+        <div class="container">
+        <form id="LoginForm" class="form">
+            <input type="text" id="username" placeholder="Usuário" required><br><br>
+            <input type="password" id="password" placeholder="Senha" required><br><br>
+            <button type="submit">Entrar</button>
+        
+        </div>
+</body>
+</html>
