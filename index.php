ca42812aa1f1acec088950181f8185d0cb461614
Matheus Eloim <matheuseloim@proleduca.com.br>
2023-12-21 16:31:58 -0300

    Ajustes projetos

--- index.php
+++ index.php
@@ -9,22 +9,20 @@
 </head>
 <body>
 
-    <header>
-        <h1>Solicitação de kits</h1>       
-    </header>
-
-    <section>        
+    <header>        
         <div class: Logo-Projeto>
             <img src="Logo_projetoPHP-12.png">
-        </div>
-        <h1>Solicitação de kits</h1>       
+        </div>    
+        <h1>Solicitação de kits</h1>   
     </header>
+
     <section>
         <div class="container">
         <form action="php/login.php" method="post" id="LoginForm" class="form">
-            <input type="text" id="login" placeholder="login" required><br><br>
-            <input type="password" id="senha" placeholder="senha" required><br><br>
-            <button type="submit">Entrar</button>        
+            <input type="text" id="login" name="login" placeholder="Login" required><br><br>
+            <input type="password" id="senha" name="senha" placeholder="Senha" required><br><br>
+            <button class="btmPadrao" type="submit">Entrar</button>   
+        </form>     
         </div>
     </section>
     <?php include "includes/footer.php"; ?> 
