5f2389db0b520996920885d49dc2845937e66a95
Rafael-freitas <rfreitas.social@gmail.com>
2024-01-01 21:54:50 -0300

    Front do Histórico e dos Kits em Uso. Problemas no back do histórico

--- historicoSolicitacoes.php
+++ historicoSolicitacoes.php
@@ -38,16 +38,37 @@
                         <?php 
                             include "conexao/conexao.php";
                             try{
-                                    $stmt = $conn -> query("SELECT * FROM kits");
-                                    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-
-                                    foreach($result as $row){
-                                        echo"<tr>";
-                                            echo"<td> Sala: "  . $row['sala'] . "</td>";
-                                            echo"<td>" . $row['descricao'] . "</td>";
-                                        echo"</tr>";          
-                                    }                  
-                                }catch(PDOException $e){echo "Falha na consulta a tabela kit do BD";}
+                                $stmt = $conn -> query("SELECT * FROM solicitacoes_kits");
+                                $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+                                
+                                foreach($result as $row){
+                                    echo"<tr>";
+                                        
+                                        $idKit = $row['id_kit'];
+                                        
+                                        $stmt = $conn -> query("SELECT * FROM kits where id =  $idKit");
+                                        $kit = $stmt -> fetch(PDO::FETCH_ASSOC);
+    
+                                        echo"<td>Sala: " .  $kit['sala'] . "</td>";
+                                        echo"<td>" .  $kit['descricao'] . "</td>";
+    
+                                        $idColaborador = $row['id_colaborador'];
+                                    
+                                        $stmt = $conn -> query("SELECT * FROM colaboradores where id = $idColaborador");
+                                        $colaborador = $stmt -> fetch(PDO::FETCH_ASSOC);
+    
+                                        echo"<td>" . $colaborador['cargo'] . ": " . $colaborador['nome'] . "</td>";
+                                        echo"<td>" . $row['data_hora_solicitacao'] . "</td>";
+                                        echo"<td>" . $row['data_hora_entrega'] . "</td>";
+                                        if ($row['baixa'] == "0"){
+                                            echo"<td> Kit em Uso </td>";
+                                        }else{
+                                            echo"<td> Kit entregue </td>";  
+                                        }
+                                    echo"</tr>";
+                                    
+                                }
+                            }catch(PDOException $e){}
 
 
 
--- includes/menu.php
+++ includes/menu.php
@@ -11,6 +11,7 @@
         <li><a href="Listadecolaboradores.php">Colaboradores</a></li>
         <li><a href="solicitacao.php">Solicitação de Kits</a></li>
         <li><a href="emUso.php">Kits em Uso</a></li>
+        <li><a href="historicoSolicitacoes.php">Histórico de Uso</a></li>
         <li><a href="php/logout.php">Sair</a></li>
 
       </ul>
